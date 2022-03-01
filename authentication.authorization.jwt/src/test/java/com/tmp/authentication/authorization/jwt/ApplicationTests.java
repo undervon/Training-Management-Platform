7c0253108ec80abb345ad1fcf6169590c18b815e
Silviu Butnaru <silviu.undervon@gmail.com>
2022-03-01 21:20:42 +0200

    Add empty spring application

---
+++ authentication.authorization.jwt/src/test/java/com/tmp/authentication/authorization/jwt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmp.authentication.authorization.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
