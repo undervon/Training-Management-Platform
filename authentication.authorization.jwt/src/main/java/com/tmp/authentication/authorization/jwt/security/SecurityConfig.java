4152beb5b0ed5e3ce91307a2245799fbfb128cc3
Silviu Butnaru <silviu.undervon@gmail.com>
2022-04-06 12:37:06 +0300

    Change authorization access

--- authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/security/SecurityConfig.java
+++ authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/security/SecurityConfig.java
@@ -62,8 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/1.0/tmp/auth/validateRefreshToken/**").permitAll()
                 .antMatchers("/api/1.0/tmp/auth/generateAccessToken/**").permitAll()
                 // Private endpoints
-                .antMatchers("/api/1.0/tmp/auth/addUser/**").permitAll()
-//                .antMatchers("/api/1.0/tmp/auth/addUser/**").hasAuthority(Roles.ADMIN.getAuthority())
+                .antMatchers("/api/1.0/tmp/auth/addUser/**").hasAuthority(Roles.ADMIN.getAuthority())
                 .antMatchers("/api/1.0/tmp/auth/editRole/**").hasAuthority(Roles.ADMIN.getAuthority())
                 .antMatchers("/api/1.0/tmp/auth/deleteRole/**").hasAuthority(Roles.ADMIN.getAuthority())
                 .anyRequest().authenticated();
