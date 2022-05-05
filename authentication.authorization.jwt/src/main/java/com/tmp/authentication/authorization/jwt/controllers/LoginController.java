affa9b0d577cb5706c2e8b9a98b8b75b1cc5632a
Silviu Butnaru <silviu.undervon@gmail.com>
2022-05-06 00:53:31 +0300

    Adding request description in swagger for LoginController class

--- authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/controllers/LoginController.java
+++ authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/controllers/LoginController.java
@@ -4,6 +4,10 @@ import com.tmp.authentication.authorization.jwt.models.UserCredentialsDTO;
 import com.tmp.authentication.authorization.jwt.models.TokenDTO;
 import com.tmp.authentication.authorization.jwt.models.TokensDTO;
 import com.tmp.authentication.authorization.jwt.services.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpMethod;
@@ -24,6 +28,14 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    @Operation(summary = "Login request")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - if successful"),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED - if the username or the password is wrong",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "NOT_FOUND - if the user not found in DB",
+                    content = @Content)
+    })
     @CrossOrigin
     @PostMapping(value = "/login",
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -36,6 +48,20 @@ public class LoginController {
                 .body(loginService.loginReq(userCredentialsDTO));
     }
 
+    @Operation(summary = "Generate access token by refresh token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK - if successful"),
+            @ApiResponse(responseCode = "404", description = "NOT_FOUND - if the user not found in DB",
+                    content = @Content),
+            @ApiResponse(responseCode = "406", description = "NOT_ACCEPTABLE - if [ the token is in BlackList ] OR "
+                    + "[ Expired JWT token ] OR "
+                    + "[ Invalid JWT token ] OR "
+                    + "[ Unacceptable JWT token ] OR "
+                    + "[ Invalid JWT signature ] OR "
+                    + "[ Unsupported JWT token ] OR "
+                    + "[ JWT claims string is empty ]",
+                    content = @Content)
+    })
     @CrossOrigin
     @PostMapping(value = "/generateAccessToken",
             consumes = MediaType.APPLICATION_JSON_VALUE,
