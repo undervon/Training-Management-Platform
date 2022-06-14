d6752cc457f6d79d4be643c76353b9810ef17ab5
Silviu Butnaru <silviu.undervon@gmail.com>
2022-06-14 15:52:03 +0300

    Update Spring Security

--- authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/security/SecurityConfig.java
+++ authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/security/SecurityConfig.java
@@ -5,15 +5,15 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Log4j2
@@ -25,17 +25,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
         jsr250Enabled = true,
         prePostEnabled = true
 )
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final JwtTokenFilter jwtTokenFilter;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> userDetailsService().loadUserByUsername(username));
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
 
         http.sessionManagement()
@@ -51,27 +52,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/1.0/tmp/auth/validateAccessToken/**").permitAll()
                 .antMatchers("/api/1.0/tmp/auth/validateRefreshToken/**").permitAll()
                 .antMatchers("/api/1.0/tmp/auth/generateAccessToken/**").permitAll()
+                .antMatchers("/api/1.0/tmp/auth/**").permitAll()
                 // Private endpoints
 //                .antMatchers("/api/1.0/tmp/auth/addUser/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/editUser/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/deleteUser/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/editRole/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/deleteRole/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/getUser/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/getUsers/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-//                .antMatchers("/api/1.0/tmp/auth/image/**").hasAuthority(RoleValue.ADMIN.getAuthority())
-                .antMatchers("/api/1.0/tmp/auth/addUser/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/editUser/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/deleteUser/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/editRole/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/deleteRole/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/getUser/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/getUsers/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/user/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/image/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/logout/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/subordinateUsers/**").permitAll()
-                .antMatchers("/api/1.0/tmp/auth/unassignedUsers/**").permitAll()
                 // Swagger UI and API Docs
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/api-tmp-auth-docs/**").permitAll()
@@ -79,12 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-    }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+        return http.build();
     }
 
     @Bean
