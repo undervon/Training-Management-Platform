60804d1b7613c66156aa67dbf165fdff90cb24fb
Silviu Butnaru <silviu.undervon@gmail.com>
2022-06-18 19:45:00 +0300

    Changes in liquibase

--- authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/entities/Certificate.java
+++ authentication.authorization.jwt/src/main/java/com/tmp/authentication/authorization/jwt/entities/Certificate.java
@@ -1,11 +1,9 @@
 package com.tmp.authentication.authorization.jwt.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
