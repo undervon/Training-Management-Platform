b501c05189275cbe41b94c96865b1835e1e4172d
Silviu Butnaru <silviu.undervon@gmail.com>
2022-06-19 01:13:27 +0300

    Completing assigned courses structure

--- assigned.courses.microservice/src/main/java/com/tmp/assigned/courses/microservice/Application.java
+++ assigned.courses.microservice/src/main/java/com/tmp/assigned/courses/microservice/Application.java
@@ -1,13 +1,18 @@
 package com.tmp.assigned.courses.microservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Assigned courses API",
+        version = "1.0",
+        description = "Assigned courses microservice documentation"))
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
