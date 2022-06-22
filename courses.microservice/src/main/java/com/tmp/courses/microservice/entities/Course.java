c5bf6a221f23c327cda0def15a857d82692e82e4
Silviu Butnaru <silviu.undervon@gmail.com>
2022-06-22 14:21:09 +0300

    Comments removed

--- courses.microservice/src/main/java/com/tmp/courses/microservice/entities/Course.java
+++ courses.microservice/src/main/java/com/tmp/courses/microservice/entities/Course.java
@@ -31,15 +31,9 @@ public class Course {
     @Column(length = 10000)
     private String description;
 
-    // EN, RO
     private String language;
     private String requirements;
 
-//    Business Operations,
-//    Engineering,
-//    Management,
-//    Programming,
-//    Sales and Marketing
     private String category;
 
     @Builder.Default
